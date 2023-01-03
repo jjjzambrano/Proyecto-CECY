a2074d67023bb0fc7a4a1106f642ced3802778b4
Joshtyn <77408789+jjjzambrano@users.noreply.github.com>
2023-01-02 23:39:24 -0500

    Feat:Implementacion Login

---
+++ Certificados-Front/src/app/login/login.component.ts
@@ -0,0 +1,39 @@
+import { HttpResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Credenciales } from './credenciales';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html'
+})
+export class LoginComponent implements OnInit {
+
+  credential: Credenciales = { username: "", password: "" };
+
+  loggedIn: boolean = false;
+  constructor(
+    private router: Router
+  ) { }
+
+  ngOnInit(): void {
+  }
+  ingresar(): void {
+        if (this.credential.username == "admin" && this.credential.password == "1234"){this.router.navigate(["layout/dashboard"]);
+        }
+      }
+  }
+  /* singIn(): void{
+     this.loginService.singIn(this.credential).subscribe(
+       (response: HttpResponse<Credenciales>) => {
+         if (response.headers.get('Authorization')!=null){
+           sessionStorage.setItem("seguridad_token", String(response.headers.get('Authorization')));
+           sessionStorage.setItem("username", this.credential.username);
+         }
+       }
+     )
+     this.loggedIn = true;
+     sessionStorage.setItem("loggedIn","true");
+     this.router.navigate(["layout"]);
+   }*/
+
